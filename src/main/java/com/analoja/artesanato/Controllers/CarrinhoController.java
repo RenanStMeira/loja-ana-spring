c872071a01431165e89f447fbd677afc19f3357a
RenanStMeira <renan.meira25@gmail.com>
2024-03-23 15:15:17 -0300

    create: controller carrinho

--- src/main/java/com/analoja/artesanato/Controllers/CarrinhoController.java
+++ src/main/java/com/analoja/artesanato/Controllers/CarrinhoController.java
@@ -1,14 +1,13 @@
 package com.analoja.artesanato.Controllers;
 
+import com.analoja.artesanato.DTO.Carrinho.CarrinhoCreateDTO;
 import com.analoja.artesanato.entity.Carrinho;
 import com.analoja.artesanato.exceptions.RegraDeNegocioException;
 import com.analoja.artesanato.services.CarrinhoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
@@ -21,5 +20,31 @@ public class CarrinhoController {
     @PostMapping
     public ResponseEntity<Carrinho> adicionarItem(Integer idProduto, Integer quantidade, Integer idCliente) throws RegraDeNegocioException {
         return ResponseEntity.ok(carrinhoService.adicionarItem( idProduto, quantidade, idCliente));
+    }
+
+    @PutMapping("/{idCarrinho}/item/{idItemCarrinho}")
+    public ResponseEntity<Carrinho> atualizarItem(@PathVariable Integer idCarrinho, @PathVariable Integer idItemCarrinho, Integer idProduto, Integer quantidade) throws RegraDeNegocioException {
+        return ResponseEntity.ok(carrinhoService.atualizarItem(idCarrinho, idItemCarrinho, idProduto, quantidade));
+    }
+
+    @DeleteMapping("/{idCarrinho}/item/{idItemCarrinho}")
+    public ResponseEntity<Carrinho> removerItem(@PathVariable Integer idCarrinho, @PathVariable Integer idItemCarrinho) throws RegraDeNegocioException {
+        return ResponseEntity.ok(carrinhoService.removerItem(idCarrinho, idItemCarrinho));
+    }
+
+    @GetMapping("/{idCarrinho}")
+    public ResponseEntity<Carrinho> buscarPorId(@PathVariable Integer idCarrinho) throws RegraDeNegocioException {
+        return ResponseEntity.ok(carrinhoService.buscarPorId(idCarrinho));
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<Carrinho> criar(@RequestBody CarrinhoCreateDTO carrinhoCreateDTO) throws RegraDeNegocioException {
+        return ResponseEntity.ok(carrinhoService.criar(carrinhoCreateDTO));
+    }
+
+    @DeleteMapping("/{idCarrinho}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer idCarrinho) {
+        carrinhoService.deletar(idCarrinho);
+        return ResponseEntity.noContent().build();
     }
 }
